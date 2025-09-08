9215d35f355290e12bdfa542e77aab109dfa2787
Manaswini Poluri <110304039+charManaswini@users.noreply.github.com>
2025-09-08 16:37:49 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -21,5 +21,11 @@ const eslintConfig = [
     ],
   },
 ];
+{
+  "extends": "next/core-web-vitals",
+  "rules": {
+    "@typescript-eslint/no-explicit-any": "off"
+  }
+}
 
 export default eslintConfig;
